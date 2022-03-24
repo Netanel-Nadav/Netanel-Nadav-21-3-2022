f93736f292133116a4f9e80dde9e8b5a879089b5
Netanel Nadav <natinadav932@gmail.com>
2022-03-24 17:34:47 +0200

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { RootCmp } from './RootCmp';
